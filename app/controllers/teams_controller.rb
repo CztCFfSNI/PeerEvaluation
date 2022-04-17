64909d5025ab4c7442cd3054b6f9f0154151a334
Sparsh Kustagishettar <kustagishettar.1@osu.edu>
2022-04-17 17:59:11 -0400

    Fixed changes in views and controllers

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -65,6 +65,6 @@ class TeamsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def team_params
-      params.require(:team).permit(:name, :team_id)
+      params.require(:team).permit(:name)
     end
 end
--- app/views/teams/_team.json.jbuilder
+++ app/views/teams/_team.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! team, :id, :name, :created_at, :updated_at, :team_id
+json.extract! team, :id, :name, :created_at, :updated_at
 json.url team_url(team, format: :json)
