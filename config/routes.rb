8b297d6d19cebca59cb1b91a80deece82fed784b
CztCFfSNI <jerrychoiczt@gmail.com>
2022-04-17 19:52:41 -0400

    add links on home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :teams
   resources :students
-  root 'students#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
