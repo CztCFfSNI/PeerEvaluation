aa26622139775fa1f4e77d04b2625aac091e7486
CztCFfSNI <jerrychoiczt@gmail.com>
2022-04-20 16:37:33 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,8 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get '/:controller/:action', to: 'application'
   get '/team/add', to: 'teams#add'
-<<<<<<< HEAD
   get '/team/remove', to: 'teams#remove'
-=======
   get '/project/add', to: 'projects#add'
->>>>>>> 63345b9b7807d7828b62b8dfb0f50520c1206efe
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
