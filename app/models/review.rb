e2d3bcce6abf6c883d6008f9e528d95de8c9ed59
Mohith Kankanala <kankanala.3@osu.edu>
2022-04-21 13:42:02 -0400

    new team validation works

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-    # validates :personalscore, presence: true {message: 'personal score needed'}, numericality: {only_integer: true}
-    # validates :workscore, presence: true {message: 'workscore needed'}, numericality: {only_integer: true}
-    # validates :comment, presence: true, length: {maxium: 150}
+    # validates :personalscore, presence: true, numericality: {only_integer: true}
+    # validates :workscore, presence: true, numericality: {only_integer: true}
+    # validates :comment, presence: true, length: {maximum: 150}
  end
  
