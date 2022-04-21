c62450da6ad11e490ad3d7981515bb5c95d039d4
CztCFfSNI <jerrychoiczt@gmail.com>
2022-04-21 02:30:52 -0400

    comment out the bugs in model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ApplicationRecord
     has_many :student_teams
     has_many :teams, through: :student_teams
-    validates :email, format: { with: /\A\S+@\S+.\S+\z/xi}
-    validates :Fname, presence: true, length: {maxium: 50}
-    validates :Lname, presence: true, length: {maxium: 50}
+    # validates :email, format: { with: /\A\S+@\S+.\S+\z/xi}
+    # validates :Fname, presence: true, length: {maxium: 50}
+    # validates :Lname, presence: true, length: {maxium: 50}
  end
  
--- app/models/student_team.rb
+++ app/models/student_team.rb
@@ -1,8 +1,8 @@
 class StudentTeam < ApplicationRecord
     belongs_to :student
     belongs_to :team
-    validates :student_id, presence: true {message: 'student_id needed'}, numericality: {only_integer: true}
-    validates :team_id, presence: true {message: 'team_id needed'}, numericality: {only_integer: true}
+    # validates :student_id, presence: true {message: 'student_id needed'}, numericality: {only_integer: true}
+    # validates :team_id, presence: true {message: 'team_id needed'}, numericality: {only_integer: true}
   
  end
  
