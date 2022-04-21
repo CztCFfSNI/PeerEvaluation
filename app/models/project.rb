c4f10e51e34993152b26a6d29a6a13a412342162
Mohith Kankanala <kankanala.3@osu.edu>
2022-04-21 13:24:05 -0400

    project validations work

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
-    validates :desc, length: { maximum: 140 }
+    
     has_many :project_teams
     has_many :teams, through: :project_teams
-    # validates :name, presence: true, length: {maxium: 50}
-    # validates :desc, presence: true, length: {maxium: 300}
+    validates :name, presence: true, length: {maximum: 50}
+    validates :desc, presence: true, length: {maximum: 300}
   
  end
  
