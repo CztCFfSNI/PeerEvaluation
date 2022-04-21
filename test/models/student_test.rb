76dd6fb4a515d4be51db48be23f946e43c5fbe69
Mohith Kankanala <kankanala.3@osu.edu>
2022-04-21 18:42:07 -0400

    added test cases for student

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
-
+ 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup
+   @student = Student.new(Fname: "Student", Lname: "Good", email: "mohith@gmail.com")
+ end
+ 
+ test "should be valid" do
+   assert @student.valid?
+ end
+ 
+ test "Fname should be present" do
+   @student.Lname = ""
+   assert_not @student.valid?
+ end
+ 
+ test "Lname should be present" do
+   @student.Lname = ""
+   assert_not @student.valid?
+ end
+ 
+ test "email should be present" do
+   @student.email = ""
+   assert_not @student.valid?
+ end
+ 
+ test "Lname should not be too long" do
+   @student.Lname = "m" * 51
+   assert_not @student.valid?
+   end
+ 
+ test "Fname should not be too long" do
+   @student.Fname = "m" * 51
+   assert_not @student.valid?
+   end
+ 
+ test "email should not be too long" do
+   @student.email = "m" * 51
+   assert_not @student.valid?
+   end
+ 
+ 
 end
